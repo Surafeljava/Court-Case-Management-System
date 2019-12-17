925db749a1a595b6d7cf849ad4b75903f4f11358
Surafel Kindu <surafelk27@gmail.com>
2019-12-16 20:44:04 -0800

    database connection on main.go

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
 
 func main() {
 	fmt.Println("Welcome To Court Case Management System")
+	db, err := gorm.Open("postgres", "host=localhost port=5433 user=postgres dbname=gormdb password=123456")
+	defer db.Close()
+
+	if err != nil {
+		panic(err)
+	}
 }
