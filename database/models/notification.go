3c241107fedcd11eb0ffa2efd65df5572bab0c44
berrybab6 <56698884+berrybab6@users.noreply.github.com>
2019-12-29 17:16:34 +0300

    Update notification.go

--- database/models/notification.go
+++ database/models/notification.go
@@ -6,8 +6,9 @@ import (
 
 type Notification struct {
 	gorm.Model
-	NotTitle string
-	NotDesc  string
-	NotLevel string
-	NotDate  string
+	NotTitle string    `gorm:"type:varchar(200);not null"`
+	NotDesc  string    `gorm:"type:varchar(1500);not null"`
+	NotLevel string    `gorm:"type:varchar(70);not null"`
+	NotDate  time.Time `gorm:"not null"`
+	//dear me!!!  remember to add &parseTime=True in the maincode
 }
