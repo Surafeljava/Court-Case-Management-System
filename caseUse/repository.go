69b96e2d78bdf70ba9a9b3d48d8b5a081cbb911c
Surafel Kindu <surafelk27@gmail.com>
2020-01-01 22:47:23 -0800

    repo

--- caseUse/repository.go
+++ caseUse/repository.go
@@ -3,6 +3,7 @@ package caseUse
 import "github.com/Surafeljava/Court-Case-Management-System/entity"
 
 type CaseRepository interface {
+	CreateCase(cased entity.Case) error
 }
 
 type LoginRepository interface {
