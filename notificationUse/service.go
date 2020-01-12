a8d2e2fcdf5274599db1af48f5fe54b6b1519827
berrybab6 <56698884+berrybab6@users.noreply.github.com>
2020-01-12 05:48:49 +0300

    Update service.go

--- notificationUse/service.go
+++ notificationUse/service.go
@@ -4,7 +4,10 @@ import entity "github.com/Surafeljava/Court-Case-Management-System/Entity"
 
 //NotificationService interface
 type NotificationService interface {
-	ViewNotification(id string) (*entity.Notification, []error)
-	PostNotification(notf *entity.Notification) (*entity.Notification, []error)
+	ViewNotification(id uint) (*entity.Notification, []error)
+	PostNotification(notf *entity.Notification) []error
 	Notifications() ([]entity.Notification, []error)
+	UpdateNotification(notf *entity.Notification) (*entity.Notification, []error)
+	DeleteNotification(id uint) []error
+}
 }
