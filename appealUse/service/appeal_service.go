9f38deb35fc4f5e32a267ff8b488c8b5c19e30c3
Ey0b <56696798+Ey0b@users.noreply.github.com>
2020-01-13 02:27:48 +1100

    Update appeal_service.go

--- appealUse/service/appeal_service.go
+++ appealUse/service/appeal_service.go
@@ -5,15 +5,3 @@ import (
 	"github.com/Surafeljava/Court-Case-Management-System/entity"
 )
 
-type AppealServiceImpl struct {
-	appealRepo appealUse.AppealRepositroy
-}
-
-func NewAppealServiceImpl(notf appealUse.AppealRepositroy) appealUse.AppealRepositroy {
-	return &AppealServiceImpl{notfRepo: notf}
-
-}
-
-func (ar *AppealServiceImpl) AppealGet(caseNum string) (*entity.Case, []error) {
-	return nil, nil
-}
