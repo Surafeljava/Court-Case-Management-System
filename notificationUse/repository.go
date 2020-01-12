e7bed7ac586258d1bd12d90743dd9bffd000afeb
berrybab6 <56698884+berrybab6@users.noreply.github.com>
2020-01-12 05:48:22 +0300

    Update repository.go

--- notificationUse/repository.go
+++ notificationUse/repository.go
@@ -4,7 +4,10 @@ import entity "github.com/Surafeljava/Court-Case-Management-System/Entity"
 
 // NotificationRepository specifies notification related database operations
 type NotificationRepository interface {
-	ViewNotification(id string) (*entity.Notification, []error)
-	PostNotification(notf *entity.Notification) (*entity.Notification, []error)
+	ViewNotification(id uint) (*entity.Notification, []error)
+	PostNotification(notf *entity.Notification) []error
 	Notifications() ([]entity.Notification, []error)
+	UpdateNotification(notf *entity.Notification) (*entity.Notification, []error)
+	DeleteNotification(id uint) []error
+}
 }
