2158881248a97291fb2369166ab3c9efbdff5b84
Khalid Mohammed Awol <cowad2@gmail.com>
2020-01-12 09:03:33 -0800

    created report_repo.go

--- reportUse/repository.go
+++ reportUse/repository.go
@@ -1,4 +1,7 @@
 package reportUse
 
+import "github.com/Surafeljava/Court-Case-Management-System/entity"
+
 type ReportRepository interface {
+	CreateCourtReport() ([]entity.Case, []entity.Judge, []error)
 }
--- reportUse/repository/report_repo.go
+++ reportUse/repository/report_repo.go
@@ -1,7 +1,19 @@
 package repository
 
-import "database/sql"
+import (
+	"github.com/Surafeljava/Court-Case-Management-System/entity"
+	"github.com/Surafeljava/gorm"
+)
 
 type ReportRepositoryImpl struct {
-	conn *sql.DB
+	conn *gorm.DB
+}
+
+// NewAppealGormRepo creates a new object of UserGormRepo
+func NewReportGormRepo(db *gorm.DB) *ReportRepositoryImpl {
+	return &ReportRepositoryImpl{conn: db}
+}
+
+func (rr *ReportRepositoryImpl) CreateCourtReport() ([]entity.Case, []entity.Judge, []error) {
+	return nil, nil, nil
 }
