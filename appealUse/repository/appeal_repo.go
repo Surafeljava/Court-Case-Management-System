1535da4c65bf087935cce89c1354301f28e32338
Ey0bMaru <eyobmaru1111@gmail.com>
2020-01-13 01:57:30 +1100

    Appeal Repository

--- appealUse/repository/appeal_repo.go
+++ appealUse/repository/appeal_repo.go
@@ -1,16 +1,53 @@
 package repository
 
-import "database/sql"
+import (
+	entity "github.com/Surafeljava/Court-Case-Management-System/Entity"
+	user "github.com/Surafeljava/Court-Case-Management-System/appealUse"
+	"github.com/Surafeljava/gorm"
+)
 
-type AppealRepositoryImpl struct {
-	conn *sql.DB
+// AppealGormRepo Implements the Repoeitory interface
+type AppealGormRepo struct {
+	conn *gorm.DB
 }
 
-func NewAppealRepositoryImpl(conn *gorm.DB) appealUse.AppealRepository {
-	return &AppealRepositoryImpl{db: conn}
+// NewAppealGormRepo creates a new object of UserGormRepo
+func NewAppealGormRepo(db *gorm.DB) user.AppealRepositroy {
+	return &AppealGormRepo{conn: db}
 }
 
-func (ar *AppealRepositoryImpl) AppealGet(caseNum string) (*entity.Case, []error){
-	return nil, nil
+// Appeal return all Cases from the database
+func (appealRepo *AppealGormRepo) Appeal(oppNum string) (*entity.Case, *entity.Opponent, *entity.Witness, *entity.Decision, []error) {
+	rel, err := appealRepo.RelationForAppeal(oppNum)
+	if len(err) > 0 {
+		return nil, nil, nil, nil, err
+	}
+
+	// Case
+	caseNum := rel.CaseNum
+	cases, err := appealRepo.CaseForAppeal(caseNum)
+	if len(err) > 0 {
+		return nil, nil, nil, nil, err
+	}
+
+	// Opponnet
+	opp, err := appealRepo.OppForAppeal(oppNum)
+	if len(err) > 0 {
+		return nil, nil, nil, nil, err
+	}
+
+	// Witness
+	wit, err := appealRepo.WitnessForAppeal(caseNum)
+	if len(err) > 0 {
+		return nil, nil, nil, nil, err
+	}
+
+	// Dicision
+	dic, err := appealRepo.DecisionForAppeal(caseNum)
+	if len(err) > 0 {
+		return nil, nil, nil, nil, err
+	}
+	return cases, opp, wit, dic, err
 }
+
 
