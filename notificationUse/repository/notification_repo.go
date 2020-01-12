06e356111d0a498a547eb9d2ec8ff0ddc6285086
berrybab6 <56698884+berrybab6@users.noreply.github.com>
2020-01-12 05:45:54 +0300

    Update notification_repo.go

--- notificationUse/repository/notification_repo.go
+++ notificationUse/repository/notification_repo.go
@@ -1,9 +1,9 @@
 package repository
 
 import (
-	"github.com/Surafeljava/Court-Case-Management-System/notificationUse"
-
 	"github.com/Surafeljava/Court-Case-Management-System/entity"
+	"github.com/Surafeljava/Court-Case-Management-System/notificationuse"
+
 	"github.com/jinzhu/gorm"
 )
 
@@ -19,36 +19,63 @@ func NewNotificationRepositoryImpl(conn *gorm.DB) notificationuse.NotificationRe
 }
 
 //Notifications shows all notification posted by admin
-func (notfRepo *NotificationRepositoryImpl) Notifications()([]entity.Notification,[]error) {
-	notifications:=[]entity.Notification{}
-	errs:=notfRepo.db.Find(&notifications).GetErrors()
-	if len(errs) > 0
-	{
-		return nil,errs
+func (notfRepo *NotificationRepositoryImpl) Notifications() ([]entity.Notification, []error) {
+	notifications := []entity.Notification{}
+	errs := notfRepo.db.Find(&notifications).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
 	}
-	return notifications,errs
+	return notifications, errs
 }
 
 //PostNotification posts notification in the database
-func (notfRepo *NotificationRepositoryImpl) PostNotification(notf *entity.Notification) (*entity.Notification,[]error){
-	notification:=notf
-	errs:=notfRepo.db.Create(notification).GetErrors()
+func (notfRepo *NotificationRepositoryImpl) PostNotification(notf *entity.Notification) []error {
+	notification := notf
+	errs := notfRepo.db.Create(notification).GetErrors()
 
-	if len(errs) > 0{
-		return nil,errs
+	if len(errs) > 0 {
+		return errs
 	}
-	return notification,errs
-	}
+	return errs
+}
 
 //ViewNotification retrieves a notification by title
-func (notfRepo *NotificationRepositoryImpl) ViewNotification(id string) (*entity.Notification,[]error){
-	notification:=entity.Notification{}
-	errs:=notfRepo.db.First(&notification,id).GetErrors
-	if len(errs)>0{
-		return nil,errs
+func (notfRepo *NotificationRepositoryImpl) ViewNotification(id uint) (*entity.Notification, []error) {
+	notification := entity.Notification{}
+	errs := notfRepo.db.First(&notification, id).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
 
 	}
-	return &notification,errs
+	return &notification, errs
 
 }
-	
+
+//UpdateNotification ...Admin  update notification
+func (notfRepo *NotificationRepositoryImpl) UpdateNotification(notf *entity.Notification) (*entity.Notification, []error) {
+	notification := notf
+	errs := notfRepo.db.Save(&notification).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs
+
+	}
+	return notification, errs
+
+}
+
+//DeleteNotification deletes a given notification by id
+func (notfRepo *NotificationRepositoryImpl) DeleteNotification(id uint) []error {
+
+	notification, errs := notfRepo.ViewNotification(id)
+
+	if len(errs) > 0 {
+		return errs
+	}
+
+	errs = notfRepo.db.Delete(notification, id).GetErrors()
+	if len(errs) > 0 {
+		return errs
+
+	}
+	return errs
+}
