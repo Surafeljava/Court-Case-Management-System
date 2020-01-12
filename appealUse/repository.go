a57778061f9c10c95faaf33c61593b1ed25aa993
Surafel Kindu <surafelk27@gmail.com>
2020-01-12 01:54:02 -0800

    structure update

--- appealUse/repository.go
+++ appealUse/repository.go
@@ -1,7 +1,9 @@
 package appealUse
 
+import "github.com/Surafeljava/Court-Case-Management-System/entity"
+
 type AppealRepositroy interface {
 	AppealGet(caseNum string) (*entity.Case, []error)
-
 }
+
 //Comment
