ed412d682d112c082841045a10d31295b98847e0
Khalid Mohammed Awol <cowad2@gmail.com>
2020-01-27 13:23:59 -0800

    report repo

--- reportUse/repository/report_repo.go
+++ reportUse/repository/report_repo.go
@@ -1,7 +1,7 @@
 package repository
 
 import (
-	"github.com/Surafeljava/Court-Case-Management-System/entity"
+	entity "github.com/Surafeljava/Court-Case-Management-System/Entity"
 	"github.com/jinzhu/gorm"
 )
 
@@ -14,6 +14,59 @@ func NewReportGormRepo(db *gorm.DB) *ReportRepositoryImpl {
 	return &ReportRepositoryImpl{conn: db}
 }
 
-func (rr *ReportRepositoryImpl) CreateCourtReport() ([]entity.Case, []entity.Judge, []error) {
-	return nil, nil, nil
+func (rr *ReportRepositoryImpl) GetClosedCases() ([]entity.Case, error) {
+	cases := []entity.Case{}
+	errs := rr.conn.Where("case_status = ?", "closed").Find(&cases).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs[0]
+	}
+	return cases, nil
+}
+func (rr *ReportRepositoryImpl) GetOpenCases() ([]entity.Case, error) {
+	cases := []entity.Case{}
+	errs := rr.conn.Where("case_status = ?", "open").Find(&cases).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs[0]
+	}
+	return cases, nil
+}
+func (rr *ReportRepositoryImpl) GetAllCases() ([]entity.Case, error) {
+	cases := []entity.Case{}
+	errs := rr.conn.Find(&cases).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs[0]
+	}
+	return cases, nil
+}
+func (rr *ReportRepositoryImpl) GetCriminalJudges() ([]entity.Judge, error) {
+	judges := []entity.Judge{}
+	errs := rr.conn.Where("judge_type = ?", "criminal").Find(&judges).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs[0]
+	}
+	return judges, nil
+}
+func (rr *ReportRepositoryImpl) GetCivilJudges() ([]entity.Judge, error) {
+	judges := []entity.Judge{}
+	errs := rr.conn.Where("judge_type = ?", "civil").Find(&judges).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs[0]
+	}
+	return judges, nil
+}
+func (rr *ReportRepositoryImpl) GetAllJudges() ([]entity.Judge, error) {
+	judges := []entity.Judge{}
+	errs := rr.conn.Find(&judges).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs[0]
+	}
+	return judges, nil
+}
+func (rr *ReportRepositoryImpl) GetAllNotifications() ([]entity.Notification, error) {
+	notifications := []entity.Notification{}
+	errs := rr.conn.Find(&notifications).GetErrors()
+	if len(errs) > 0 {
+		return nil, errs[0]
+	}
+	return notifications, nil
 }
