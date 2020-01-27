233bdf73483b5d64159c8204016319d8cbdf025f
Khalid Mohammed Awol <cowad2@gmail.com>
2020-01-27 13:20:58 -0800

    report service completed

--- reportUse/service.go
+++ reportUse/service.go
@@ -3,5 +3,11 @@ package reportUse
 import entity "github.com/Surafeljava/Court-Case-Management-System/Entity"
 
 type ReportService interface {
-	CreateCourtReport() ([]entity.Case, []entity.Judge, []error)
+	GetClosedCases() ([]entity.Case, error)
+	GetOpenCases() ([]entity.Case, error)
+	GetAllCases() ([]entity.Case, error)
+	GetCriminalJudges() ([]entity.Judge, error)
+	GetCivilJudges() ([]entity.Judge, error)
+	GetAllJudges() ([]entity.Judge, error)
+	GetAllNotifications() ([]entity.Notification, error)
 }
