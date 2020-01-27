f91dd9ffd32bdbd3b2797c91ab911a60f5a9a060
berrybab6 <56698884+berrybab6@users.noreply.github.com>
2020-01-27 14:25:57 +0300

    Update Notification Handler

--- court/handler/notification_handler.go
+++ court/handler/notification_handler.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 	"time"
 
 	entity "github.com/Surafeljava/Court-Case-Management-System/Entity"
@@ -24,12 +26,69 @@ func NewNotificationHandler(T *template.Template, NotfServ notificationUse.Notif
 func (nh *NotificationHandler) AdminUpdateNotification(w http.ResponseWriter, r *http.Request) {
 	//To Do ..|
 	//update notification goes here
+	if r.Method == http.MethodGet {
+
+		idRaw := r.URL.Query().Get("id")
+
+		id, err := strconv.Atoi(idRaw)
+
+		if err != nil {
+			fmt.Println("Can't get the id!")
+			panic(err)
+		}
+
+		notif, _ := nh.notfService.ViewNotification(uint(id))
+
+		nh.tmpl.ExecuteTemplate(w, "admin.notifications.update.layout", notif)
+
+	} else if r.Method == http.MethodPost {
+
+		id, _ := strconv.Atoi(r.FormValue("id"))
+		notTitle := r.FormValue("not_title")
+		notLevel := r.FormValue("not_level")
+		notDescription := r.FormValue("not_desc")
+
+		notification := entity.Notification{ID: uint(id), NotDescription: notDescription, NotTitle: notTitle, NotLevel: notLevel, NotDate: time.Now()}
+
+		_, err := nh.notfService.UpdateNotification(&notification)
+
+		if len(err) > 0 {
+			fmt.Println(">> -- >> -- >> error on sending Notification to the updateNotification func")
+			panic(err)
+		}
+
+		http.Redirect(w, r, "/admin/notifications", http.StatusSeeOther)
+
+	} else {
+
+		http.Redirect(w, r, "/admin/notifications", http.StatusSeeOther)
+
+	}
+}
+
+// AdminNotifications handle requests on route /admin/notifications
+func (nh *NotificationHandler) AdminNotifications(w http.ResponseWriter, r *http.Request) {
+	notifications, errs := nh.notfService.Notifications()
+	if len(errs) > 0 {
+		return
+	}
+
+	notificationStore := []entity.Notification{}
+
+	for i := 0; i < len(notifications); i++ {
+
+		notificationStore = append(notificationStore, notifications[i])
+
+	}
+	nh.tmpl.ExecuteTemplate(w, "admin.notifications.layout", notificationStore)
+
 }
 
 // AdminPostNotification handles POST coming from admin url request
 func (nh *NotificationHandler) AdminPostNotification(w http.ResponseWriter, r *http.Request) {
+
 	if r.Method == http.MethodPost {
-		r.ParseForm()
+		//r.ParseForm()
 		notTitle := r.FormValue("not_title")
 		notLevel := r.FormValue("not_level")
 		notDescription := r.FormValue("not_desc")
@@ -38,12 +97,37 @@ func (nh *NotificationHandler) AdminPostNotification(w http.ResponseWriter, r *h
 		errs := nh.notfService.PostNotification(&notification)
 
 		if len(errs) > 0 {
-			return
+			panic(errs)
 		}
-		http.Redirect(w, r, "/admin/home", http.StatusSeeOther)
 
+		http.Redirect(w, r, "/admin/notifications", http.StatusSeeOther)
 	} else {
-		nh.tmpl.ExecuteTemplate(w, "admin.postNotification.html", nil)
+		nh.tmpl.ExecuteTemplate(w, "admin.notifications.postnotification.layout", nil)
 	}
 
 }
+
+//AdminDeleteNotification existing Notification
+func (nh *NotificationHandler) AdminDeleteNotification(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+
+		idRaw := r.URL.Query().Get("id")
+
+		id, err := strconv.Atoi(idRaw)
+
+		if err != nil {
+			fmt.Println("Can't get the id!")
+			panic(err)
+		}
+
+		errs := nh.notfService.DeleteNotification(uint(id))
+
+		if len(errs) > 0 {
+			http.Redirect(w, r, "/admin/notifications", http.StatusSeeOther)
+		}
+
+		http.Redirect(w, r, "/admin/notifications", http.StatusSeeOther)
+
+	}
+}
+
