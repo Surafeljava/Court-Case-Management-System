48c3f524d914c075d63a6823f3ddd3f413b3737d
Daniel Tefera Deti <danieltefera2211@gmail.com>
2020-01-28 23:24:32 +0300

    Appeal handler

--- court/handler/opp_appeal_hundler.go
+++ court/handler/opp_appeal_hundler.go
@@ -49,33 +49,6 @@ func (oppAp *AppealHandler) OppAppeal(w http.ResponseWriter, r *http.Request) {
 
 	tmplAppeal.ExecuteTemplate(w, "appeal.layout", data)
 	return
-	oppNum := r.FormValue("opp_appeal")
-	cases, opp, wit, dic, err := oppAp.appealService.Appeal(oppNum)
-
-	data := entity.AppealForm{
-		CaseNum:          cases.CaseNum,
-		CaseCreationDate: cases.CaseCreation,
-		CaseTitle:        cases.CaseTitle,
-		CaseDesc:         cases.CaseDesc,
-		OppName:          opp.OppName,
-		OppGender:        opp.OppGender,
-		OppAddress:       opp.OppAddress,
-		OppPhone:         opp.OppPhone,
-		WitDocm:          wit.WitnessDoc,
-		WitTy:            wit.WitnessType,
-		Decision:         dic.Decision,
-		DecDate:          dic.DecisionDate,
-		DacDesc:          dic.DecisionDesc,
-	}
-
-	if len(err) > 0 {
-		w.Header().Set("Content-Type", "application/json")
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		return
-	}
-
-	tmplAppeal.ExecuteTemplate(w, "appeal.layout", data)
-	return
 }
 
 //OppTrial is only for trial
