f22388316fca47215e0afb31fca6da70bc62d218
Surafel Kindu <surafelk27@gmail.com>
2020-01-27 23:01:54 -0800

    admin court handler

--- court/handler/admin_court_handler.go
+++ court/handler/admin_court_handler.go
@@ -17,13 +17,36 @@ func NewAdminCourtHandler(T *template.Template, JS caseUse.CourtService) *AdminC
 }
 
 func (ach *AdminCourtHandler) CreateCourt(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		//Check if court information has created before....
+	}
 
+	if r.Method == http.MethodPost {
+		//Create a new court
+
+	}
 }
 
 func (ach *AdminCourtHandler) UpdateCourt(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		//Check if court information has created before....
+		//If there is get the court info for editing...
+	}
 
+	if r.Method == http.MethodPost {
+		//update the court info
+
+	}
 }
 
 func (ach *AdminCourtHandler) AdminCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		//Check if admin has created before....
 
+	}
+
+	if r.Method == http.MethodPost {
+		//Create a new admin
+
+	}
 }
